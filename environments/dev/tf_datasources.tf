ca3dda64b131b1e221ef5d8810c886d663c81d09
cloudnativesystem <cloud.native.system@gmail.com>
2022-07-19 04:49:13 -0400

    Deploying Dev EKS Control Plane

--- environments/dev/tf_datasources.tf
+++ environments/dev/tf_datasources.tf
@@ -1,5 +1,5 @@
 # remote state
-/*
+
 data "terraform_remote_state" "remote_state" {
   backend = "s3"
 
@@ -10,5 +10,34 @@ data "terraform_remote_state" "remote_state" {
   }
 }
 
-*/
+
 #-----------------------------------------------------------------------------------------------------
+
+
+# eks
+
+data "aws_caller_identity" "current" {}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+#------------------------------------------------------------------------------------------------------
