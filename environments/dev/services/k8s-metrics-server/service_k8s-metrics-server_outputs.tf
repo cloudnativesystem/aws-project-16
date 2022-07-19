f44d22d2f05615bd1bf000162df13af4ae91a808
cloudnativesystem <cloud.native.system@gmail.com>
2022-07-19 06:00:54 -0400

    removed outputs

--- environments/dev/services/k8s-metrics-server/service_k8s-metrics-server_outputs.tf
+++ environments/dev/services/k8s-metrics-server/service_k8s-metrics-server_outputs.tf
@@ -1,11 +1,3 @@
 output "metrics_server_manifest" {
   value = helm_release.metrics-server.manifest
 }
-
-output "metrics_server_status" {
-  value = helm_release.metrics-server.metadata.status
-}
-
-output "metrics_server_version" {
-  value = helm_release.metrics-server.metadata.version
-}
