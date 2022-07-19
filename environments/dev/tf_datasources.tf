d33449d589aac0fb9b5d6f9481b9150b688bfc1b
cloudnativesystem <cloud.native.system@gmail.com>
2022-07-19 04:37:17 -0400

    Deploying Dev VPC Service

--- environments/dev/tf_datasources.tf
+++ environments/dev/tf_datasources.tf
@@ -4,9 +4,9 @@ data "terraform_remote_state" "remote_state" {
   backend = "s3"
 
   config = {
-    bucket = "BUCKET"
-    key    = "BUCKET/dev/tf.state"
-    region = "REGION"
+    bucket = "onekloud-swagwatch-infra"
+    key    = "onekloud-swagwatch-infra/dev/tf.state"
+    region = "us-east-2"
   }
 }
 
